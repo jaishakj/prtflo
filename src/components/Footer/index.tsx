5cb4d88d039b1d7e1e7510989e99d5c2234bafce
Jaishak J <99122415+jaishakj@users.noreply.github.com>
2026-09-01 23:53:14 +0530

    Add Footer component with copyright information

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>© 2025 Chennai — Built by Jaishak J</p>
+      </div>
+    </footer>
+  );
+}
